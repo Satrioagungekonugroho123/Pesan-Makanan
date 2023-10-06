da6ada7cc5453a6508b518305a40f72df3565a4b
Satrio Agung Eko Nugroho_2215101063 <129135406+Satrioagungekonugroho123@users.noreply.github.com>
2023-10-06 22:02:39 +0700

    Add files via upload

---
+++ Web Pesan Makanan/src/componen.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="app">
+    <img alt="vue logo" src="product.image">
+    logo
+</div>
+
+
+<script>
+    import logo from './componen/logo.vue'
+
+export default {
+    name: 'app',
+    componen: {
+        logo
+    }
+}
+</script>
+
+<style>
+
+    #app {
+<font-family></font-family>
+
+
+    }
+
+
+
+
+
+</style>
+
+</template>
